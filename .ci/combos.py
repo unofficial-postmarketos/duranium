d105c18fe03434f81b7728412b6bd1e46d17669a
Clayton Craft <clayton@craftyguy.net>
2025-08-10 20:46:06 -0700

    ci: add config to build images
    Signed-off-by: Clayton Craft <craftyguy@postmarketos.org>

---
+++ .ci/combos.py
@@ -0,0 +1,16 @@
+ARCH_COMBOS = {
+    "aarch64": [
+        {"device": "lenovo-21bx", "ui": "cosmic", "release": "edge"},
+        {"device": "lenovo-21bx", "ui": "gnome", "release": "edge"},
+        {"device": "lenovo-21bx", "ui": "plasma-desktop", "release": "edge"},
+    ],
+    "x86_64": [
+        {"device": "generic-x86_64", "ui": "cosmic", "release": "edge"},
+        {"device": "generic-x86_64", "ui": "gnome", "release": "edge"},
+        {"device": "generic-x86_64", "ui": "plasma-desktop", "release": "edge"},
+
+        {"device": "google-x64cros", "ui": "cosmic", "release": "edge"},
+        {"device": "google-x64cros", "ui": "gnome", "release": "edge"},
+        {"device": "google-x64cros", "ui": "plasma-desktop", "release": "edge"},
+    ],
+}
---
+++ .ci/generate_image_jobs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+from jinja2 import Template
+
+sys.path.insert(0, str(Path(__file__).parent))
+from combos import ARCH_COMBOS
+
+if len(sys.argv) != 3:
+    print("usage: generate_image_jobs.py TEMPLATE OUTPUT")
+    sys.exit(1)
+
+template = Template(Path(sys.argv[1]).read_text())
+output = template.render(arch_combos=ARCH_COMBOS)
+Path(sys.argv[2]).write_text(output)
