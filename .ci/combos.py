521e07f18cbbf1bd3c42f76022bf778726456478
Clayton Craft <craftyguy@postmarketos.org>
2026-02-18 13:54:39 -0800

    ci/combos: add fairphone-fp5 + phosh and plamo
    Signed-off-by: Clayton Craft <craftyguy@postmarketos.org>

--- .ci/combos.py
+++ .ci/combos.py
@@ -1,5 +1,7 @@
 ARCH_COMBOS = {
     "aarch64": [
+        {"device": "fairphone-fp5", "ui": "phosh", "release": "edge"},
+        {"device": "fairphone-fp5", "ui": "plasma-mobile", "release": "edge"},
         {"device": "lenovo-21bx", "ui": "cosmic", "release": "edge"},
         {"device": "lenovo-21bx", "ui": "gnome", "release": "edge"},
         {"device": "lenovo-21bx", "ui": "plasma-desktop", "release": "edge"},
