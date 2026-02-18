9cd91c6c6fa4c795ad3cae0783fb57523d56074c
Clayton Craft <craftyguy@postmarketos.org>
2026-02-18 13:52:37 -0800

    ci/deploy-image: prune old images before uploading new
    Signed-off-by: Clayton Craft <craftyguy@postmarketos.org>

--- .ci/deploy-image.sh
+++ .ci/deploy-image.sh
@@ -10,6 +10,27 @@ scp -P "$SSH_PORT" "$SSH_HOST:/var/www/duranium.postmarketos.org/images/$IMAGE_I
 # Verify existing manifest if signature exists
 if [ -f SHA256SUMS.old.gpg ]; then gpg --verify SHA256SUMS.old.gpg SHA256SUMS.old; fi
 
+# Prune old images, keeping (IMAGES_TO_KEEP - 1) old versions to make room for the new one
+versions=$(grep " ${IMAGE_ID}_" SHA256SUMS.old | sed "s/.*${IMAGE_ID}_//" | cut -d. -f1 | sort -u)
+if [ -n "$versions" ]; then
+	total=$(echo "$versions" | wc -l)
+	keep=$((IMAGES_TO_KEEP - 1))
+	prune_count=$((total - keep))
+	if [ $prune_count -gt 0 ]; then
+		prune_versions=$(echo "$versions" | head -n $prune_count)
+		for version in $prune_versions; do
+			# collect files for this version from the manifest
+			files=$(grep " ${IMAGE_ID}_${version}\." SHA256SUMS.old | awk '{print $2}')
+			# delete from server
+			for f in $files; do
+				ssh -p "$SSH_PORT" "$SSH_HOST" "rm -f /var/www/duranium.postmarketos.org/images/$IMAGE_ID/$f"
+			done
+			# remove from manifest
+			sed -i "/ ${IMAGE_ID}_${version}\./d" SHA256SUMS.old
+		done
+	fi
+fi
+
 # Merge manifests
 mv mkosi.output/*/SHA256SUMS SHA256SUMS.new
 cat SHA256SUMS.new SHA256SUMS.old > SHA256SUMS
